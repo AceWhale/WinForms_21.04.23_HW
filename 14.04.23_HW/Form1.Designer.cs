baeeaa3815a55710cf2f0842e6f62e8dfcc27cf0
Nikita Mohseni <mohseninikita9@gmail.com>
2023-04-17 23:54:04 +0300

    Доделал

--- 14.04.23_HW/Form1.Designer.cs
+++ 14.04.23_HW/Form1.Designer.cs
@@ -46,13 +46,19 @@
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -117,6 +123,7 @@
             this.button6.Size = new System.Drawing.Size(72, 44);
             this.button6.TabIndex = 6;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
@@ -140,6 +147,7 @@
             this.button4.Size = new System.Drawing.Size(72, 44);
             this.button4.TabIndex = 4;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // menuStrip1
             // 
@@ -165,6 +173,7 @@
             this.администраторскийРежимToolStripMenuItem.Name = "администраторскийРежимToolStripMenuItem";
             this.администраторскийРежимToolStripMenuItem.Size = new System.Drawing.Size(217, 24);
             this.администраторскийРежимToolStripMenuItem.Text = "Администраторский режим";
+            this.администраторскийРежимToolStripMenuItem.Click += new System.EventHandler(this.администраторскийРежимToolStripMenuItem_Click);
             // 
             // оПрограммеToolStripMenuItem
             // 
@@ -270,15 +279,16 @@
             this.panel1.Size = new System.Drawing.Size(420, 132);
             this.panel1.TabIndex = 11;
             // 
-            // pictureBox1
+            // label2
             // 
-            this.pictureBox1.BackgroundImage = global::_14._04._23_HW.Properties.Resources.zvonok;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(143, 134);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(148, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Максим";
             // 
             // label1
             // 
@@ -291,16 +301,67 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Максим";
             // 
-            // label2
+            // pictureBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.ForeColor = System.Drawing.Color.Gold;
-            this.label2.Location = new System.Drawing.Point(148, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 22);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Максим";
+            this.pictureBox1.BackgroundImage = global::_14._04._23_HW.Properties.Resources.zvonok;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(143, 134);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(602, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Gold;
+            this.label6.Location = new System.Drawing.Point(4, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "label6";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Gold;
+            this.label5.Location = new System.Drawing.Point(4, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Gold;
+            this.label4.Location = new System.Drawing.Point(4, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Gold;
+            this.label3.Location = new System.Drawing.Point(4, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
             // 
             // Form1
             // 
@@ -310,6 +371,7 @@
             this.BackgroundImage = global::_14._04._23_HW.Properties.Resources.mil;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1095, 683);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
@@ -333,6 +395,8 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -361,6 +425,11 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- 14.04.23_HW/Form1.cs
+++ 14.04.23_HW/Form1.cs
@@ -10,22 +10,27 @@ using System.Windows.Forms;
 using System.IO;
 using System.Threading;
 using System.Windows.Forms.VisualStyles;
+using System.Media;
 
 namespace _14._04._23_HW
 {
     public partial class Form1 : Form
     {
         string cor_answ;
-        string[] lines;
+        public string[] lines;
         List<string> answers, shuffle;
         Random rand = new Random();
         int count = 0, answ = 14;
         List<Button> buttons;
+        List<Label> labels;
+        SoundPlayer player = new SoundPlayer();
         public Form1()
         {
             InitializeComponent();
+            panel2.Visible = false;
             panel1.Visible = false;
             buttons = new List<Button> { button7, button8, button9, button10};
+            labels = new List<Label> { label3, label4, label5, label6 };
             BackgroundImage = Properties.Resources.mil;
             string[] strings = { "15 - 1 000 000", "14 - 500 000", "13 - 250 000" , "12 - 125 000", "11 - 64 000" , "10 - 32 000",
             "9 - 16 000", "8 - 8 000", "7 - 4 000", "6 - 2 000", "5 - 1 000", "4 - 500", "3 - 300", "2 - 200", "1 - 100"};
@@ -57,15 +62,25 @@ namespace _14._04._23_HW
         }
         private void Game(bool check)
         {
+            panel1.Visible = false;
+            panel2.Visible = false;
             foreach (Button x in buttons)
             {
                 x.Enabled = true;
                 x.BackColor = Color.Black;
             }
-            Thread.Sleep(2000);
+            if (answ < 0)
+            {
+                button3_Click(null, null);
+                player = new SoundPlayer(@"C:\Users\mohse\source\repos\14.04.23_HW\14.04.23_HW\res\sound\winner.wav");
+                player.Play();
+                return;
+            }
             if (check)
             {
-                panel1.Visible = false;
+                player = new SoundPlayer(@"C:\Users\mohse\source\repos\14.04.23_HW\14.04.23_HW\res\sound\true.wav");
+                player.Play();
+                Thread.Sleep(4000);
                 textBox1.Text = lines[count];
                 cor_answ = lines[count + 1];
                 answers = new List<string> { lines[count + 1], lines[count + 2], lines[count + 3], lines[count + 4] };
@@ -80,10 +95,15 @@ namespace _14._04._23_HW
             }
             else
             {
+                player = new SoundPlayer(@"C:\Users\mohse\source\repos\14.04.23_HW\14.04.23_HW\res\sound\false.wav");
+                player.Play();
+                Thread.Sleep(4000);
+                groupBox1.Enabled = false;
                 foreach (Button x in buttons)
                     x.Enabled = false;
                 textBox1.Text = "You lose";
                 listBox1.SelectedIndex = -1;
+                button3.Enabled = false;
             }
         }
 
@@ -99,6 +119,8 @@ namespace _14._04._23_HW
 
         private void button5_Click(object sender, EventArgs e)
         {
+            player = new SoundPlayer(@"C:\Users\mohse\source\repos\14.04.23_HW\14.04.23_HW\res\sound\zvonok.wav");
+            player.Play();
             panel1.Visible= true;
             label2.Text = "Я думаю это...\n";
             int rand_a = rand.Next(1, 100);
@@ -113,8 +135,76 @@ namespace _14._04._23_HW
             button5.BackgroundImage = Properties.Resources._5;
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            player = new SoundPlayer(@"C:\Users\mohse\source\repos\14.04.23_HW\14.04.23_HW\res\sound\gong.wav");
+            player.Play();
+            Button cor_but = new Button();
+            foreach (Button x in buttons)
+                if (x.Text == cor_answ)
+                    cor_but = (Button)x;
+            foreach (Button x in buttons)
+                x.Enabled = false;
+            cor_but.Enabled = true;
+            while (true)
+            {
+                int rand_b = rand.Next(0, 3);
+                if (buttons[rand_b].Text != cor_answ)
+                {
+                    buttons[rand_b].Enabled = true;
+                    break;
+                }
+            }
+            button4.BackgroundImage = Properties.Resources._4;
+            button4.Enabled=false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            player = new SoundPlayer(@"C:\Users\mohse\source\repos\14.04.23_HW\14.04.23_HW\res\sound\zal.wav");
+            player.Play();
+            panel2.Visible = true;
+            int chance = 100;
+            int cor_anws_c = rand.Next(30, 80);
+            chance -= cor_anws_c;
+            int rand_answ;
+            for (int i = 0; i < 4; i++)
+            {
+                if (buttons[i].Text == cor_answ)
+                    labels[i].Text = $"{buttons[i].Text} - {cor_anws_c}%";
+                else
+                {
+                    rand_answ = rand.Next(0, chance);
+                    labels[i].Text = $"{buttons[i].Text} - {rand_answ}%";
+                    if (chance - rand_answ < 0)
+                        chance = 0;
+                    else
+                        chance -= rand_answ;
+                }
+            }
+            button6.Enabled=false;
+            button6.BackgroundImage= Properties.Resources._6;
+            button4.Select();
+        }
+
+        private void администраторскийРежимToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Check check = new Check();
+            check.frm = this;
+            Hide();
+            check.Show();
+        }
+        public void AdminCreate()
+        {
+            Admin admin = new Admin();
+            admin.frm = this;
+            admin.Show();
+            admin.Start(lines);
+        }
         private void button3_Click(object sender, EventArgs e)
         {
+            player = new SoundPlayer(@"C:\Users\mohse\source\repos\14.04.23_HW\14.04.23_HW\res\sound\summa.wav");
+            player.Play();
             foreach (Button x in buttons)
                 x.Enabled = false;
             string text = "Вы выиграли";
